ba3380b4210539b2ad448be1428fa8e261424b02
RaphaelleC <raphaelle.charrier@gmail.com>
2021-09-15 18:11:46 +0100

    basic database, express installed

---
+++ db/data/animals.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    number: 1,
+    name: '',
+    race: '',
+    personality: '',
+    availability: '',
+    picture: 'url',
+  },
+  {
+    number: 2,
+    name: '',
+    race: '',
+    personality: '',
+    availability: '',
+    picture: 'url',
+  },
+  {
+    number: 3,
+    name: '',
+    race: '',
+    personality: '',
+    availability: '',
+    picture: 'url',
+  },
+  {
+    number: 4,
+    name: '',
+    race: '',
+    personality: '',
+    availability: '',
+    picture: 'url',
+  },
+  {
+    number: 5,
+    name: '',
+    race: '',
+    personality: '',
+    availability: '',
+    picture: 'url',
+  }
+]
